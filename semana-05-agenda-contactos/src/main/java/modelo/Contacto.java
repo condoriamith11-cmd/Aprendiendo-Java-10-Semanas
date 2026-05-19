08b8434e684ca3093202a358fe964030508dc99f
condoriamith11-cmd <condoriamith11@gmail.com>
2026-05-19 17:00:53 -0400

    feat: clase Contacto con encapsulamiento y validaciones

---
+++ semana-05-agenda-contactos/src/main/java/modelo/Contacto.java
@@ -0,0 +1,50 @@
+package modelo;
+
+import exception.DatoInvalidoException;
+
+public class Contacto {
+    private String id;
+    private String nombre;
+    private String telefono;
+
+    public Contacto(String id, String nombre, String telefono){
+        setId(id);
+        setNombre(nombre);
+        setTelefono(telefono);
+    }
+    
+    public String getId(){
+        return id;
+    }
+    public Stirng getNombre(){
+        return nombre;  
+    }
+    public String getTelefono(){
+        return telefono;
+    }
+
+    public void setId(String id){
+        if(id == null || id.trim().isEmpty()){
+            throw new DatoInvalidoException("id", "no puede estar vacio");
+        }
+        this.id = id.trim();
+    }
+
+    public void setNombre(String nombre){
+        if(nombre == null || nombre.trim().isEmpty()){
+            throw new DatoInvalidoException("nombre", "no puede estar vacio");
+        }
+        this.nombre = nombre.trim();
+    }
+
+    public void setTelefono(String telefono){
+        if(telefono == null || telefono.length() < 7){
+            throw new DatoInvalidoException("telefono", "debe tener al menos 7 digitos");
+        }
+        this.telefono = telefono;
+    }
+
+    public String toString(){
+        return String.format("[%s] %s - Tel: %s", id, nombre, telefono);
+    }
+}
