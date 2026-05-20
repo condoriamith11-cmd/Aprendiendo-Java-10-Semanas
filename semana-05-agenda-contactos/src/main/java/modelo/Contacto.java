9f353861b6b6000c07420c6b72dcfa719251658e
condoriamith11-cmd <condoriamith11@gmail.com>
2026-05-19 21:20:51 -0400

    feat: clase Contacto con encapsulamiento y validaciones( agregando email)

--- semana-05-agenda-contactos/src/main/java/modelo/Contacto.java
+++ semana-05-agenda-contactos/src/main/java/modelo/Contacto.java
@@ -6,11 +6,13 @@ public class Contacto {
     private String id;
     private String nombre;
     private String telefono;
+    private String email;
 
-    public Contacto(String id, String nombre, String telefono){
+    public Contacto(String id, String nombre, String telefono, String email){
         setId(id);
         setNombre(nombre);
         setTelefono(telefono);
+        setEmail(email);
     }
     
     public String getId(){
@@ -22,7 +24,9 @@ public class Contacto {
     public String getTelefono(){
         return telefono;
     }
-
+    public String getEmail(){
+        return email;
+    }
     public void setId(String id){
         if(id == null || id.trim().isEmpty()){
             throw new DatoInvalidoException("id", "no puede estar vacio");
@@ -44,7 +48,19 @@ public class Contacto {
         this.telefono = telefono;
     }
 
+    public void setEmail(String email){
+    if(email == null || email.trim().isEmpty()){
+        throw new DatoInvalidoException("email", "no puede estar vacio");
+    }
+
+    if(!email.contains("@")){
+        throw new DatoInvalidoException("email", "debe contener @");
+    }
+
+    this.email = email.trim();
+    }
+
     public String toString(){
-        return String.format("[%s] %s - Tel: %s", id, nombre, telefono);
+    return String.format("[%s] %s - Tel: %s - Email: %s", id, nombre, telefono, email);
     }
 }
