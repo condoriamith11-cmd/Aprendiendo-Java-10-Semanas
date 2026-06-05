62c77aea91a89b24516d56c68b881c6dd1b81096
condoriamith11-cmd <condoriamith11@gmail.com>
2026-06-05 15:49:09 -0400

    feat: MenuBar con RouterLink a Inicio y Contactos

--- semana-08-agenda-web/src/main/java/com/organizacion/app/ui/MainLayout.java
+++ semana-08-agenda-web/src/main/java/com/organizacion/app/ui/MainLayout.java
@@ -5,12 +5,14 @@ import com.organizacion.app.views.InicioView;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.menubar.MenuBar;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.router.RouterLink;
 
 public class MainLayout extends AppLayout{
     public MainLayout(){
-        H2 titulo = new H2("Mi Aplicacion");
+        H2 titulo = new H2("Agenda de Contactos");
+
         MenuBar menu = new MenuBar();
         menu.addItem(new RouterLink("Inicio", InicioView.class));
         menu.addItem(new RouterLink("Contactos", ContactosView.class));
@@ -18,7 +20,7 @@ public class MainLayout extends AppLayout{
         HorizontalLayout barra = new HorizontalLayout(titulo, menu);
         barra.setWidthFull();
         barra.expand(titulo);
-        barra.setAlignItems(com.vaadin.flow.component.orderedlayout.FlexComponent.Alignment.CENTER);
+        barra.setAlignItems(FlexComponent.Alignment.CENTER);
 
         addToNavbar(barra);
     }
